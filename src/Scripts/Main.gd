f360d2a0106716a6fcbcce096386fb6f87bb8e4d
Credulidad <jesusjeampoolm@gmail.com>
2020-10-06 11:37:32 -0500

    Database implemented and connected

--- src/Scripts/Main.gd
+++ src/Scripts/Main.gd
@@ -1,13 +1,92 @@
 extends Node
 
+var option = 0
+var state = 0
+var content
+
 func _ready():
 	$Options/Background.color = Color8(1,1,1,0)
 	$Options/Background.rect_position.x = -1080
 	$Options/NavbarL.rect_position.x = -800
-	
 
 func _on_TextureButton_pressed():
 	($AnimationPlayer as AnimationPlayer).play("navbarShow")
 
 func _on_ExitOptions_pressed():
 	($AnimationPlayer as AnimationPlayer).play("navbarHide")
+
+func _plusActive():
+	pass
+
+func _on_Home_pressed():
+	option=1
+	print(option)
+	#asd
+
+func _on_Ingresos_pressed():
+	option=2
+	print(option)
+	#asd
+
+func _on_Gastos_pressed():
+	option=3
+	print(option)
+	#asd
+
+func _on_Inversiones_pressed():
+	option=4
+	print(option)
+	#asd
+
+func _on_semana_pressed():
+	option=5
+	print(option)
+	#asd
+
+func _on_Base_pressed():
+	option=6
+	print(option)
+	var s = ResourceLoader.load("res://src/scenes/databaseRead.tscn") # Load the new scene.
+	content = s.instance() # Instanciar una escena
+	$Main/content.add_child(content) # Add it to the active scene, as child of root.
+	($AnimationPlayer as AnimationPlayer).play("navbarHide")
+
+func _on_Opciones_pressed():
+	option=7
+	print(option)
+	#asd
+	
+func _on_Salir_pressed():
+	get_tree().quit()
+	
+func _on_Plus_pressed():
+	if option==1:
+		pass
+	elif option==6:
+		var s = ResourceLoader.load("res://src/scenes/form.tscn") # Load the new scene.
+		content = s.instance() # Instanciar una escena
+		$Main/content.add_child(content) # Add it to the active scene, as child of root.
+		$Main/Bar/Plus.disabled=true
+	
+	_plus()
+
+func _on_back_pressed():
+	if option==1:
+		pass
+	elif option==6 and state==0:
+		pass
+	elif option==6 and state==1:
+		$Main/content/Form/AnimationPlayer.play("finish")
+		yield(get_tree().create_timer(0.3),"timeout")
+		$Main/content/Form.queue_free()
+	state=0
+	_plus()
+	
+func _plus():
+	$Main/Bar/Plus.disabled=false
+	if ($Main/Bar/Plus.disabled==true):
+		$Main/Bar/Plus.disabled=true
+		$Main/Bar/Plus.visible=false
+	elif ($Main/Bar/Plus.disabled==false):
+		$Main/Bar/Plus.disabled=false
+		$Main/Bar/Plus.visible=true
--- src/Scripts/formulario.gd
+++ src/Scripts/formulario.gd
@@ -1,12 +1,17 @@
 extends Node
 
+var numData
 var num = 0
 var data = ["Nombre","Descripcion","Costo"]
 
+var dataRead
+
 func _ready():
 	($Nombre/AnimationPlayer as AnimationPlayer).play("experimental")
+	if get_tree().get_nodes_in_group("main"):
+		get_parent().get_parent().get_parent().get_parent().state=1
 
-func focus_entered():
+func focus_entered(): #Focus dinamico a base de la señal del focus entered en el textedit de los nodos
 	if (get_node(data[num]).get_node("TextEdit"+str(num)).text == ""):
 		(get_node(data[num]).get_node("AnimationPlayer") as AnimationPlayer).play("in")
 		return
@@ -14,38 +19,56 @@ func focus_entered():
 	if (get_node(data[num]).get_node("ColorRect").color == Color8(187,222,251,255)):
 		(get_node(data[num]).get_node("AnimationPlayer") as AnimationPlayer).play("select")
 	
-func focus_exited():
+func focus_exited(): #Focus dinamico a base de la señal del focus exited en el textedit de los nodos
 	if (get_node(data[num]).get_node("TextEdit"+str(num)).text==""):
 		(get_node(data[num]).get_node("AnimationPlayer") as AnimationPlayer).play("outIncorrect")
 		return
 		
 	get_node(data[num]).get_node("ColorRect").color = Color8(187,222,251,255)
 
+#### Focus ##########################
+func _on_TextEdit0_focus_entered():##
+	num = 0                        ##
+	focus_entered()                ##
+								   ##
+func _on_TextEdit0_focus_exited(): ##
+	focus_exited()                 ##
+								   ##
+func _on_TextEdit1_focus_entered():##
+	num = 1                        ##
+	focus_entered()                ##
+								   ##
+func _on_TextEdit1_focus_exited(): ##
+	focus_exited()                 ##
+								   ##
+func _on_TextEdit2_focus_entered():##
+	num = 2                        ##
+	focus_entered()                ##
+								   ##
+func _on_TextEdit2_focus_exited(): ##
+	focus_exited()                 ##
+#####################################
 
-func _on_TextEdit0_focus_entered():
-	num = 0
-	focus_entered()
-
-func _on_TextEdit0_focus_exited():
-	focus_exited()
-
-func _on_TextEdit1_focus_entered():
-	num = 1
-	focus_entered()
-
-func _on_TextEdit1_focus_exited():
-	focus_exited()
-
-func _on_TextEdit2_focus_entered():
-	num = 2
-	focus_entered()
-
-func _on_TextEdit2_focus_exited():
-	focus_exited()
-
-
-func _on_Button_pressed():
+func _on_Button_pressed(): #Boton para subir los datos a la base de datos
+	#Si nombre o costo estan vacios no hagas nada y corta la ejecucion
 	if (get_node("Nombre/TextEdit0").text == "") or (get_node("Costo/TextEdit2").text == ""):
 		print("Faltan datos")
 		return
-	print("Falta programar la base de datos")
+		
+	#Actualiza los datos de la base de datos
+	$"/root/Database".data.nombre = $Nombre/TextEdit0.text 
+	$"/root/Database".data.descripcion = $Descripcion/TextEdit1.text 
+	$"/root/Database".data.costo = $Costo/TextEdit2.text
+	
+	var s = ResourceLoader.load("res://src/scenes/data.tscn") # Load the new scene.
+	if get_tree().get_nodes_in_group("main"):
+		numData = get_parent().get_parent().get_node("databaseRead").num
+	dataRead = s.instance() # Instanciar una escena
+	dataRead.get_node("nombre").text = $Nombre/TextEdit0.text 
+	dataRead.get_node("descripcion").text = $Descripcion/TextEdit1.text 
+	dataRead.get_node("costo").text = $Costo/TextEdit2.text
+	dataRead.name = "data"+str(numData)
+	if get_tree().get_nodes_in_group("main"):
+		get_parent().get_parent().get_node("databaseRead/ScrollContainer/Formulario").add_child(dataRead)
+	
+	Database.save_data()
